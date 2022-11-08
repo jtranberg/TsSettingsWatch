b332fb9c8386cd8d165a5d02a72d9db3eb27bbf4
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-11-07 22:40:40 -0800

    using ..watch mode..--w...--watch

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+const norma = {
+    breed: "silkie",
+    eggsPerWeek: 4,
+    name: "Norma",
+    age: 2
+};
+const bertha = {
+    breed: "silkie",
+    eggsPerWeek: 5,
+    name: "Bertha",
+    age: 1
+};
---
+++ index.ts
@@ -0,0 +1,18 @@
+interface Chicken {
+    breed: string;
+    eggsPerWeek: number;
+    name: string;
+    age: number;
+}
+const norma: Chicken = {
+    breed: "silkie",
+    eggsPerWeek: 4,
+    name: "Norma",
+    age:2
+}
+const bertha: Chicken = {
+    breed: "silkie",
+    eggsPerWeek: 5,
+    name: "Bertha",
+    age:1
+}
